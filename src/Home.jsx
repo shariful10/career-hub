346969a003fe7def2e6a35eac45010560eced619
Sk Shariful Islam <skshariful10@gmail.com>
2023-04-10 12:39:02 +0600

    Favicon Added

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 			<div className="my-container grid sm:grid-cols-2 gap-12 md:gap-16">
 				<div className="order-2 sm:order-1 mb-5 md:mb-0">
 					<h1 className="text-4xl md:text-6xl lg:text-[80px] font-bold">
-						One Step Closer To Your <span className="gradient-text">Dream Job</span>
+						One Step Closer To Your <span className="text-gradient">Dream Job</span>
 					</h1>
 					<p className="text-[18px] text-[#757575] mt-6 mb-8">
 						Explore thousands of job opportunities with all the information you need.
